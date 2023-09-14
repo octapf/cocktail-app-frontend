21265bafc3f15cd2f476bc20f0e5ca98eb3016c7
octapf <frangipani.octavio@gmail.com>
2023-09-14 21:09:53 +0200

    add navbar icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import '../styles/navbar.css'
 
 const NavBar = () => {
-	return <nav className='navbar'>Nav Bar</nav>
+	return (
+		<nav className='navbar'>
+			<i className='fa-solid fa-house'></i>
+			<i className='fa-solid fa-heart'></i>
+			<i className='fa-solid fa-magnifying-glass'></i>
+			<i className='fa-regular fa-user'></i>
+		</nav>
+	)
 }
 
 export default NavBar
