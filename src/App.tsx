dec424be5089b87ec8310aaea5ccf64a87e21e6e
octapf <frangipani.octavio@gmail.com>
2023-09-19 10:53:06 +0200

    fix width size

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 		return (
 			<>
 				<div className='background-home'></div>
-				<Container typeContainer='container-home'>
+				<Container typeContainer='contanier-logo'>
 					<Outlet />
 				</Container>
 			</>
