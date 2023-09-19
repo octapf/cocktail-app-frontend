52386dd67be1ea3052d08c2e7b306fc7d4badb9f
octapf <frangipani.octavio@gmail.com>
2023-09-19 13:04:15 +0200

    fix opacity infocontainer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,7 @@ const Card = ({ product }: ICard) => {
 
 	return (
 		<Container typeContainer={`container-card ${productClass}`}>
+			<div className='background-infocard'></div>
 			<Container typeContainer={'container-infocard'}>
 				<h5>{product.name}</h5>
 				<h5>€ {product.price}</h5>
