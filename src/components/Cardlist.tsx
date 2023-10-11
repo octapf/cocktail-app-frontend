e6034bb0f1a8390e28c4f09225c2cf4a21d99807
octapf <frangipani.octavio@gmail.com>
2023-10-11 13:03:20 +0200

    fix return loading msj

--- src/components/Cardlist.tsx
+++ src/components/Cardlist.tsx
@@ -14,65 +14,62 @@ const Cardlist = () => {
 	const searchParam = searchParams.get('search')
 	const alcoholParam = searchParams.get('alcohol')
 
-	if (products.length > 1) {
-		let productsContent: any[] = []
-		if (searchParam !== null && searchParam.length > 0) {
-			console.log('SEARCH PARAM')
+	if (products.length < 1) return <>Loading</>
+	let productsContent: any[] = []
+	if (searchParam !== null && searchParam.length > 0) {
+		console.log('SEARCH PARAM')
 
-			productsContent = products
-				.filter((prod) =>
-					prod.name.toLowerCase().match(searchParam.toLowerCase())
-				)
-				.map((prod) => {
-					return (
-						<Link key={prod._id} to={`${endpoints.COCKTAIL_LIST}/${prod._id}`}>
-							<Card productName={prod.name} />
-						</Link>
-					)
-				})
-		}
-
-		if (alcoholParam !== null) {
-			const filteredByAlcohol = products.filter((prod) =>
-				prod.ingredients.some((el) =>
-					el.name.toLowerCase().match(new RegExp(`^${alcoholParam}$`))
-				)
+		productsContent = products
+			.filter((prod) =>
+				prod.name.toLowerCase().match(searchParam.toLowerCase())
 			)
-
-			console.log(filteredByAlcohol)
-			console.log('ALCOHOL PARAM')
-
-			productsContent = filteredByAlcohol.map((prod) => {
+			.map((prod) => {
 				return (
 					<Link key={prod._id} to={`${endpoints.COCKTAIL_LIST}/${prod._id}`}>
 						<Card productName={prod.name} />
 					</Link>
 				)
 			})
-		}
-
-		if (searchParam == null && alcoholParam == null) {
-			console.log('NO PARAM')
-
-			productsContent = products.map((prod) => {
-				return (
-					<Link key={prod._id} to={`${endpoints.COCKTAIL_LIST}/${prod._id}`}>
-						<Card productName={prod.name} />
-					</Link>
-				)
-			})
-		}
-
-		return (
-			<main className='main'>
-				<Container typeContainer={'container-cardlist'}>
-					{productsContent}
-				</Container>
-			</main>
-		)
-	} else {
-		return <>Loading</>
 	}
+
+	if (alcoholParam !== null) {
+		const filteredByAlcohol = products.filter((prod) =>
+			prod.ingredients.some((el) =>
+				el.name.toLowerCase().match(new RegExp(`^${alcoholParam}$`))
+			)
+		)
+
+		console.log(filteredByAlcohol)
+		console.log('ALCOHOL PARAM')
+
+		productsContent = filteredByAlcohol.map((prod) => {
+			return (
+				<Link key={prod._id} to={`${endpoints.COCKTAIL_LIST}/${prod._id}`}>
+					<Card productName={prod.name} />
+				</Link>
+			)
+		})
+	}
+
+	if (searchParam == null && alcoholParam == null) {
+		console.log('NO PARAM')
+
+		productsContent = products.map((prod) => {
+			return (
+				<Link key={prod._id} to={`${endpoints.COCKTAIL_LIST}/${prod._id}`}>
+					<Card productName={prod.name} />
+				</Link>
+			)
+		})
+	}
+
+	return (
+		<main className='main'>
+			<Container typeContainer={'container-cardlist'}>
+				{productsContent}
+			</Container>
+		</main>
+	)
 }
 
 export default Cardlist
