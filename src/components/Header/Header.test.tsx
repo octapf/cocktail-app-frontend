36a883c33826cee91713c4fd8117c081aa293744
octapf <frangipani.octavio@gmail.com>
2023-11-02 23:37:35 +0100

    add testing to header component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,17 +1,49 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Header from './Header'
 import { BrowserRouter } from 'react-router-dom'
 
 describe('Header', () => {
-	test('render correctly', () => {
-		const app = render(
+	const MockHeader = () => {
+		return (
 			<BrowserRouter>
 				<Header />
 			</BrowserRouter>
 		)
+	}
 
-		const link = app.getByRole('link', { name: /logo cocktail bar/i })
+	beforeEach(() => {
+		render(<MockHeader />)
+	})
+
+	test('renders correctly', () => {
+		const link = screen.getByRole('link', { name: /logo cocktail bar/i })
 
 		expect(link).toBeInTheDocument()
+	})
+
+	test('should render as visible', () => {
+		const link = screen.getByRole('link', { name: /logo cocktail bar/i })
+		expect(link).toBeVisible()
+	})
+
+	/* test('should not be on the DOM when clicked', () => {
+		const link = screen.getByRole('link', { name: /logo cocktail bar/i })
+
+		fireEvent.click(link)
+
+		expect(link).not.toBeInTheDocument()
+	}) */
+
+	test('contains an image tag with class "logo-navbar"', () => {
+		const image = screen.getByRole('img', { name: /logo/i })
+
+		expect(image).toContainHTML('img')
+		expect(image).toHaveClass('logo-navbar')
+	})
+
+	test('contains an h1 tag', () => {
+		const heading = screen.getByRole('heading', { name: /cocktail bar/i })
+
+		expect(heading).toContainHTML('h1')
 	})
 })
